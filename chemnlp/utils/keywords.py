e068eead5e8ba708285275e0163ee22272ed5ea8
kellml <40702433+kellml@users.noreply.github.com>
2022-08-10 15:32:59 -0400

    Create keywords.py

---
+++ chemnlp/utils/keywords.py
@@ -0,0 +1,9 @@
+# Dictionaries with keywords for research areas:
+
+def keywords():
+  keys = {'solarMaterials': ['solar', 'power conversion efficiency', 'pce', 'schottky-queisser limit'],
+         'magneticMaterials': ['magnetic', 'hysteresis', 'curie', 'magneton', 'coercive field', 'susceptibility'],
+         'batteries': [],
+         'semiconductors': [],
+         'alloys': [],
+         ''}

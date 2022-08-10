cd64045e15e4c4301bd58d3f72e8e636251d9ded
Kamal Choudhary <writetokamal.1989@gmail.com>
2022-08-10 07:45:20 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="atombench",  
+    version="2022.7.15",
+    author="Kamal Choudhary",
+    author_email="kamal.choudhary@nist.gov",
+    description="atombench",
+    install_requires=[
+        "numpy>=1.22.0",
+        "scipy>=1.6.3",
+        "jarvis-tools>=2021.07.19",
+        "torch>=1.7.1",
+        # "torchvision>=0.10.0+cu111",
+        "scikit-learn>=0.24.1",
+        "matplotlib>=3.4.1",
+        "seaborn>=0.11.2",
+        "tqdm>=4.60.0",
+        "pandas>=1.2.3",
+        "pytorch-ignite>=0.5.0.dev20210429",
+        "pydantic>=1.8.1",
+        "flake8>=3.9.1",
+        "pycodestyle>=2.7.0",
+        "pydocstyle>=6.0.0",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/usnistgov/atomvision",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)

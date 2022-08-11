34ad25db41f5696553d4ef426c11e6e1664b6e29
kellml <40702433+kellml@users.noreply.github.com>
2022-08-11 14:41:06 -0400

    Update keywords.py

--- chemnlp/utils/keywords.py
+++ chemnlp/utils/keywords.py
@@ -1,9 +1,27 @@
 # Dictionaries with keywords for research areas:
 
 def keywords():
-  keys = {'solarMaterials': ['solar', 'power conversion efficiency', 'pce', 'schottky-queisser limit'],
+  keys = {'solarMaterials': ['solar', 'power conversion efficiency', 'pce', 'schottky-queisser limit', 'open circuit voltage', 
+                            'short circuit current', 'isc', 'voc', 'fill factor', 'diffusion length', 'absorption coefficient',
+                            'transmission', 'scatter', 'photon', 'renewable', 'photovoltaic', 'infrared', 'tandem solar cell',
+                             'lifetime', 'diffusion coefficient', 'impedance spectroscopy', 'optical interference', 'drift',
+                            'schottky', 'junction', 'ohmic', 'heterojunction', 'homojunction', 'band alignment', 'band bending',
+                            'current density', 'solar simulator', 'spectral irradiance', 'conductivity', 'resistivity',
+                            'sheet resistance'],
          'magneticMaterials': ['magnetic', 'hysteresis', 'curie', 'magneton', 'coercive field', 'susceptibility'],
-         'batteries': [],
-         'semiconductors': [],
-         'alloys': [],
-         ''}
+         'batteryMaterials': ['cathode', 'anode', 'electrode', 'electrolyte', 'capacity', 'energy density', 'cycle lifetime',
+                      'separator', 'ionic conductivity', 'electric vehicle', 'battery', 'energy storage', 'nernst',
+                      'faraday', 'faradaic charge transfer', 'pseudocapacitor', 'pseudocapacitance', 'diffusion-limited',
+                       'surface-limited', 'electrochemistry', 'electrochemical', 'supercapacitor', 'ion mobility',
+                       'diffusion coefficient', 'ac impedance', 'electrochemical impedance spectroscopy', 'electrochemistry',
+                      'overpotential', 'reduction potential', 'oxidation potential', 'redox', 'current density', 'dendrite',
+                      'cycle number', 'cycle life', 'discharge cycle'],
+         '2DMaterials': ['interlayer excitions', 'exciton binding energy', 'flexible', 'vdw', 'exfoliate', 'cvd', 'trion', 'van der waals',
+                        'mixed-dimensional heterostructure', 'excitonics', 'valleytronics', 'flexible electronics', 'wearable', 'graphene',
+                        'tmd', 'chalcogenide', 'mxene', 'xene', 'mof', 'perovskite', 'cof', 'spintronics', 'cswap', 'threshold voltage',
+                        'vth', 'electrostatic doping', 'defect passivation', 'metal organic framework', 'covalent organic framework',
+                        'moire superlattice', 'bilayer', 'trilayer', 'monolayer', 'mechanical exfoliation'],
+          'photocatalystMaterials': ['charge transfer', 'inorganic coordination complex', 'transition metal', 'phthalocyanine',
+                                    'singlet state', 'triplet state'],
+          'bioimagingMaterials': ['fluorophore', 'fluorescence'],
+         }

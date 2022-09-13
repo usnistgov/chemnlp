7258e92deebe7d10b230debc1edaf517fefd2cb3
knc6 <writetokamal.1989@gmail.com>
2022-09-13 08:15:08 -0400

    Initial commit for tools.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="chemnlp",
-    version="2022.8.11a",
+    version="2022.9.7",
     author="Kamal Choudhary, Mathew Kelley",
     author_email="kamal.choudhary@nist.gov",
     description="chemnlp",

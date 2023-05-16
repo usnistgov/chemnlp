05c9b7b050bc94708aba8e8416c9085beba2da32
knc6 <writetokamal.1989@gmail.com>
2023-05-16 17:13:01 -0400

    Add docstring.

--- chemnlp/tests/test_utils.py
+++ chemnlp/tests/test_utils.py
@@ -2,6 +2,7 @@ from chemnlp.utils.process_doc import ProcessDoc
 
 
 def test_formula():
+    """Test formula."""
     txt = "MgB2 is a superconductor."
     pdoc = ProcessDoc(text=txt)
     print(pdoc.get_chemical_formulae())

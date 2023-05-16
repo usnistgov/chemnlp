78335326a96350b16fbc0cf0404f1438dc6fd9c6
knc6 <writetokamal.1989@gmail.com>
2023-05-16 17:33:13 -0400

    Add setup.

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setuptools.setup(
     author_email="kamal.choudhary@nist.gov",
     description="chemnlp",
     install_requires=[
-<<<<<<< HEAD
         "numpy>=1.22.0",
         "scipy>=1.6.3",
         "jarvis-tools>=2021.07.19",
@@ -19,16 +18,6 @@ setuptools.setup(
         "flake8>=3.9.1",
         "pycodestyle>=2.7.0",
         "pydocstyle>=6.0.0",
-=======
-       "numpy>=1.22.0",
-       "scipy>=1.6.3",
-       "jarvis-tools>=2021.07.19",
-       "ChemDataExtractor>=1.3.0",
-       "matplotlib>=3.4.1",
-       "flake8>=3.9.1",
-       "pycodestyle>=2.7.0",
-       "pydocstyle>=6.0.0",
->>>>>>> 54b236a4b5d43c6c2fb1a8877b9d380e1dceeda0
     ],
     scripts=["chemnlp/utils/run_chemnlp.py"],
     long_description=long_description,

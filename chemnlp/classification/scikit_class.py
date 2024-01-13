f5a48beec1d07f7dc48d78b75d27af1e36378f29
Mustapha Zakaria MERABET <65148928+zaki1003@users.noreply.github.com>
2024-01-14 00:50:15 +0100

    Update scikit_class.py

--- chemnlp/classification/scikit_class.py
+++ chemnlp/classification/scikit_class.py
@@ -1,18 +1,25 @@
 """Module for classification tasks."""
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_selection import chi2
+from sklearn.feature_selection import SelectKBest, chi2, f_classif, mutual_info_classif
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.svm import LinearSVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import argparse
+import xgboost as xgb
 import sys
+from sklearn.decomposition import TruncatedSVD
 
 # from sklearn.ensemble import GradientBoostingClassifier
 import seaborn as sns
@@ -56,44 +63,6 @@ parser.add_argument(
     help="Whether or not shuffle the rows in csv before splitting",
 )
 
-
-def generate_data(n_entries=None, nentry_each=None, filename="cond_mat.csv"):
-    """Generate cond-mat dataset."""
-    d = data("arXiv")
-    df = pd.DataFrame(d)
-    cond_mat_topics = [
-        "cond-mat.mtrl-sci",
-        "cond-mat.mes-hall",
-        "cond-mat.str-el",
-        "cond-mat.stat-mech",
-        "cond-mat.supr-con",
-        "cond-mat.soft",
-        "cond-mat.quant-gas",
-        "cond-mat.other",
-        "cond-mat.dis-nn",
-    ]
-    df_cond_mat = df[
-        df["categories"].apply(
-            lambda x: "cond-mat" in x and x in cond_mat_topics
-        )
-    ]
-    if n_entries is not None:
-        df_cond_mat = df_cond_mat[0:n_entries]
-    if nentry_each is not None:
-        x = []
-        for i in cond_mat_topics:
-            x.extend(
-                df_cond_mat[df_cond_mat["categories"] == i][0:nentry_each][
-                    "id"
-                ].values
-            )
-        df_cond_mat = df_cond_mat[df_cond_mat["id"].isin(x)]
-
-    dff = df_cond_mat[["id", "title", "abstract", "categories"]]
-    dff["title_abstract"] = dff["title"] + " " + df["abstract"]
-    dff = dff.sample(frac=1, replace=True, random_state=1)
-    dff.to_csv(filename)
-    # print(len(set(dff.categories)))
 
 
 def sk_class(
@@ -181,52 +150,156 @@ def sk_class(
     info["train"] = train_info
     info["test"] = test_info
     dumpjson(data=info, filename="arXiv_categories.json")
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+    #model.fit(X_train, y_train)
+    #y_pred = model.predict(X_test)
     f = open("pred_test.csv", "w")
     # f.write(str(accuracy_score(y_test, y_pred)))
     # f.close()
     line = "id,target,prediction\n"
     f.write(line)
     # pickle.dump(model, open("log.pk", "wb"))
-    for i, j, k in zip(df.id[indices_test], y_test, y_pred):
-        # print(i,j,k)
-        line = (
-            str(i)
-            + ","
-            + str(id_to_category[int(j)])
-            + ","
-            + str(id_to_category[int(k)])
-            + "\n"
-        )
-        f.write(line)
-    f.close()
+
     # print ('indices_train',df.id[indices_train]
     # .astype(str),len(indices_train))
     # print('indices_test',df.id[indices_test].astype(str),len(indices_test))
-    print("Logistic model accuracy", accuracy_score(y_test, y_pred))
-    plt.rcParams.update({"font.size": 20})
-    conf_mat = confusion_matrix(y_test, y_pred)  # ,labels=category_to_id)
-    fig, ax = plt.subplots(figsize=(16, 16))
-    # print('category_id_df[key].values',category_id_df[key].values)
-    sns.heatmap(
-        conf_mat / conf_mat.sum(axis=1)[:, np.newaxis],
-        annot=True,
-        # fmt="d",
-        fmt=".1%",
-        cbar=False,
-        square=True,
-        cmap=sns.diverging_palette(20, 220, n=200),
-        # xticklabels=category_id_df[k].values,
-        # yticklabels=category_id_df[k].values,
-        xticklabels=category_to_id,
-        yticklabels=category_to_id,
-    )
-    plt.ylabel("Actual")
-    plt.xlabel("Predicted")
-    plt.tight_layout()
-    plt.savefig("conf_log.pdf")
-    plt.close()
+    #print("Logistic model accuracy", accuracy_score(y_test, y_pred))
+
+    #svd = TruncatedSVD(n_components=15, random_state=42)
+    #title_features = svd.fit_transform(title_features)     
+
+
+
+
+   # print("Training SVC:")
+   # model = LinearSVC(verbose=True)
+   # model.fit(X_train, y_train)
+   # y_pred = model.predict(X_test)
+    #print("SVC", accuracy_score(y_test, y_pred))
+
+
+   # Perform chi-square feature selection
+    k_best = 2000  # Number of features to select
+    
+    #chi2_selector = SelectKBest(chi2, k=k_best)
+    #chi2_selector = SelectKBest(mutual_info_classif, k=k_best)
+    #chi2_selector = SelectKBest(f_classif, k=k_best)
+    #X_train = chi2_selector.fit_transform(X_train, y_train)
+    #X_test = chi2_selector.transform(X_test)
+
+ 
+    print("Training MLPClassifier:")
+    # MLPClassifier()
+
+    model = MLPClassifier(solver='adam',activation='relu', alpha=1e-2, hidden_layer_sizes=(17), random_state=1,learning_rate='adaptive')
+
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    f = open("accuracy_MLPClassifier", "w")
+    f.write(str(accuracy_score(y_test, y_pred)))
+    f.close()
+    print("MLPClassifier", accuracy_score(y_test, y_pred))
+
+
+
+
+
+    print("Training MLPClassifier2:")
+    # MLPClassifier()
+
+    model = MLPClassifier(hidden_layer_sizes=(150,100,50),
+                        max_iter = 300,activation = 'relu',
+                        solver = 'adam')
+
+    model.fit(trainX_scaled, y_train)
+
+    y_pred = model.predict(X_test)
+    f = open("accuracy_MLPClassifier2", "w")
+    f.write(str(accuracy_score(y_test, y_pred)))
+    f.close()
+    print("MLPClassifier2", accuracy_score(y_test, y_pred))
+
+
+    print("Training KNN:")
+   
+    #chi2_selector = SelectKBest(chi2, k=k_best)
+    #chi2_selector = SelectKBest(mutual_info_classif, k=k_best)
+    chi2_selector = SelectKBest(f_classif, k=k_best)
+    X_train_knn = chi2_selector.fit_transform(X_train, y_train)
+    X_test_knn = chi2_selector.transform(X_test)
+
+ 
+
+    model = KNeighborsClassifier(n_neighbors=3)
+    model.fit(X_train_knn, y_train)
+    y_pred = model.predict(X_test_knn)
+    f = open("accuracy_KNN", "w")
+    f.write(str(accuracy_score(y_test, y_pred)))
+    f.close()
+    print("KNN", accuracy_score(y_test, y_pred))
+
+
+
+
+
+
+
+    print("Training RandomForestClassifier:")
+    # RandomForestClassifier()
+
+    model = RandomForestClassifier()
+
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+
+    print("RandomForestClassifier", accuracy_score(y_test, y_pred))
+
+
+    print("Training XGBoost:")
+    # XGBoost()
+
+    xgb.set_config(verbosity=2)     
+    xgb_model = xgb.XGBClassifier(objective="multi:softprob", random_state=42)
+    xgb_model.fit(X_train, y_train)
+
+    y_pred = xgb_model.predict(X_test)
+
+    print("XGBoost", accuracy_score(y_test, y_pred))
+
+
+
+
+    print("Training logistic regression:")
+    # LinearSVC()
+    model = LogisticRegression(random_state=0,verbose=1)  
+  
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    f = open("accuracy_logistic", "w")
+    f.write(str(accuracy_score(y_test, y_pred)))
+    f.close()
+    print("Logistic", accuracy_score(y_test, y_pred))
+
+
+
+
+
+
+
+
+    print("Training MultiNomial:")
+    # GradientBoostingClassifier()
+    model = MultinomialNB()
+
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    f = open("accuracy_MultinomialNB", "w")
+    f.write(str(accuracy_score(y_test, y_pred)))
+    f.close()
+
+
+    print("MultinomialNB", accuracy_score(y_test, y_pred))
+
+
 
 
 def scikit_classify(
@@ -300,59 +373,11 @@ def scikit_classify(
                     "\n. ".join(bigrams[-N:])
                 )
             )
-    if model_selection:
-        models = [
-            # GradientBoostingClassifier(),
-            RandomForestClassifier(),
-            # n_estimators=200, max_depth=3, random_state=0),
-            LinearSVC(),
-            MultinomialNB(),
-            LogisticRegression(random_state=0),
-            # try: multi_class='multinomial',
-            # solver='lbfgs'...same accuracy actually
-        ]
 
-        cv_df = pd.DataFrame(index=range(CV * len(models)))
-        entries = []
-        for model in models:
-            model_name = model.__class__.__name__
-            accuracies = cross_val_score(
-                model, title_features, title_labels, scoring="accuracy", cv=CV
-            )  # features: ; labels:
-            for fold_idx, accuracy in enumerate(accuracies):
-                entries.append((model_name, fold_idx, accuracy))
-        cv_df = pd.DataFrame(
-            entries, columns=["model_name", "fold_idx", "accuracy"]
-        )
+    #svd = TruncatedSVD(n_components=15, random_state=42)
+    #title_features = svd.fit_transform(title_features)     
 
-        print(cv_df)
-        # Visualize model accuracy with box plot and strip plot:
 
-        plt.figure(figsize=(10, 8))
-        plt.rcParams.update({"font.size": 18})
-        sns.boxplot(x="model_name", y="accuracy", data=cv_df)
-        sns.stripplot(
-            x="model_name",
-            y="accuracy",
-            data=cv_df,
-            size=8,
-            jitter=True,
-            edgecolor="gray",
-            linewidth=2,
-        )
-
-        plt.ylim(0, 1)
-
-        # plt.title('Model Benchmark Comparison from Title Text Data')
-        plt.ylabel("Accuracy (cross-validation score)")
-        # plt.xlabel('Model')
-        plt.xlabel("")
-        plt.xticks(rotation=45)
-        plt.tight_layout()
-        plt.savefig("cv.pdf")
-        plt.close()
-    print("Training logistic regression:")
-    model = LogisticRegression(random_state=0)  # LinearSVC()
     (
         X_train,
         X_test,
@@ -366,78 +391,96 @@ def scikit_classify(
         df.index,
         test_size=test_size,
         random_state=0,
+        shuffle=shuffle,
     )
+
+    print("Training SVC:")
+    model = LinearSVC(verbose=True)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print("SVC", accuracy_score(y_test, y_pred))
+
+
+   # Perform chi-square feature selection
+    k_best = 1500  # Number of features to select
+    
+    #chi2_selector = SelectKBest(chi2, k=k_best)
+    chi2_selector = SelectKBest(mutual_info_classif, k=k_best)
+    X_train = chi2_selector.fit_transform(X_train, y_train)
+    X_test = chi2_selector.transform(X_test)
+
+ 
+
+    print("Training XGBoost:")
+    # XGBoost()
+
+    xgb.set_config(verbosity=2)     
+    xgb_model = xgb.XGBClassifier(objective="multi:softprob", random_state=42)
+    xgb_model.fit(X_train, y_train)
+
+    y_pred = xgb_model.predict(X_test)
+
+    print("XGBoost", accuracy_score(y_test, y_pred))
+
+
+
+    print("Training MLPClassifier:")
+    # MLPClassifier()
+
+    model = MLPClassifier(solver='adam',activation='relu', alpha=1e-2, hidden_layer_sizes=(5, 2), random_state=1)
+
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    f = open("accuracy_MLPClassifier", "w")
+    f.write(str(accuracy_score(y_test, y_pred)))
+    f.close()
+    print("MLPClassifier", accuracy_score(y_test, y_pred))
+
+
+
+    print("Training logistic regression:")
+    # LinearSVC()
+    model = LogisticRegression(random_state=0,verbose=1)  
+  
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     f = open("accuracy_logistic", "w")
     f.write(str(accuracy_score(y_test, y_pred)))
     f.close()
     print("Logistic", accuracy_score(y_test, y_pred))
-    pickle.dump(model, open("log.pk", "wb"))
 
-    plt.rcParams.update({"font.size": 20})
-    conf_mat = confusion_matrix(y_test, y_pred)
-    fig, ax = plt.subplots(figsize=(16, 16))
-    sns.heatmap(
-        conf_mat / conf_mat.sum(axis=1)[:, np.newaxis],
-        annot=True,
-        # fmt="d",
-        fmt=".1%",
-        cbar=False,
-        square=True,
-        cmap=sns.diverging_palette(20, 220, n=200),
-        xticklabels=category_id_df[key].values,
-        yticklabels=category_id_df[key].values,
-    )
-    plt.ylabel("Actual")
-    plt.xlabel("Predicted")
-    plt.tight_layout()
-    plt.savefig("conf_log.pdf")
-    plt.close()
 
-    print("Training SVC:")
-    model = LinearSVC()
-    (
-        X_train,
-        X_test,
-        y_train,
-        y_test,
-        indices_train,
-        indices_test,
-    ) = train_test_split(
-        title_features,
-        title_labels,
-        df.index,
-        test_size=test_size,
-        random_state=0,
-    )
+    print("Training KNN:")
+   
+
+    model = KNeighborsClassifier(n_neighbors=3)
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    print("SVC", accuracy_score(y_test, y_pred))
-    pickle.dump(model, open("svc.pk", "wb"))
-
-    plt.rcParams.update({"font.size": 20})
-    conf_mat = confusion_matrix(y_test, y_pred)
-    fig, ax = plt.subplots(figsize=(16, 16))
-    sns.heatmap(
-        conf_mat / conf_mat.sum(axis=1)[:, np.newaxis],
-        annot=True,
-        # fmt="d",
-        fmt=".1%",
-        cbar=False,
-        square=True,
-        cmap=sns.diverging_palette(20, 220, n=200),
-        xticklabels=category_id_df[key].values,
-        yticklabels=category_id_df[key].values,
-    )
-    plt.ylabel("Actual")
-    plt.xlabel("Predicted")
-    plt.tight_layout()
-    plt.savefig("conf_svc.pdf")
-    plt.close()
-    f = open("accuracy_svc", "w")
+    f = open("accuracy_KNN", "w")
     f.write(str(accuracy_score(y_test, y_pred)))
     f.close()
+    print("KNN", accuracy_score(y_test, y_pred))
+
+
+
+
+
+
+
+    print("Training MultiNomial:")
+    # GradientBoostingClassifier()
+    model = MultinomialNB()
+
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    f = open("accuracy_MultinomialNB", "w")
+    f.write(str(accuracy_score(y_test, y_pred)))
+    f.close()
+
+
+    print("MultinomialNB", accuracy_score(y_test, y_pred))
+
+
 
 
 if __name__ == "__main__":
@@ -460,5 +503,5 @@ if __name__ == "__main__":
         min_df=min_df,
         shuffle=shuffle,
     )
-    # df = pd.read_csv("cond_mat.csv")
-    # scikit_classify(df=df, key="categories", value="title_abstract")
+    #df = pd.read_csv(csv_path)
+    #scikit_classify(df=df, key="categories", value="title_abstract")
